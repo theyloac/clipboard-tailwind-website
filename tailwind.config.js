670eb5634927c73e5d19fc21d03d75f9b0ea8830
Theyloac <theyloac@gmail.com>
2024-08-01 14:39:57 -0400

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    content: ["./**/*.{html,js}"],
+    theme: {
+        screens: {
+            sm: "480px",
+            md: "768px",
+            lg: "976px",
+            xl: "1440px",
+        },
+        extend: {
+            colors: {
+                // Add this line
+                strongCyan: "hsl(171, 66%, 44%)",
+                lightBlue: "hsl(233, 100%, 69%)",
+                darkGrayishBlue: "hsl(210, 10%, 33%)",
+                grayishBlue: "hsl(201, 11%, 66%)",
+            },
+            fontFamily: {
+                sans: ["Bai Jamjuree", "sans-serif"],
+            },
+        },
+    },
+    plugins: [],
+};
